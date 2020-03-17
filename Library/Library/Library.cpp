5ec0d5cf921eafe9164e59912134adbc800f2c34
Nicole Tilbe <nicolecali2406@outlook.com>
2020-03-17 18:55:17 -0400

    updated library.cpp

--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -11,9 +11,9 @@ int main()
     do
     {
         std::cout << "1. Add a book to the library\n";
-        std::cout << "2.Check out a new book\n";
-        std::cout << "3.Check in a book\n";
-        std::cout << "4.Display list of books from inventory : \n";
+        std::cout << "2. Check out a new book\n";
+        std::cout << "3. Check in a book\n";
+        std::cout << "4. Display list of books from inventory : \n";
         std::cout << "5. Display number of books in the library.\n";
         std::cout << "6. Exit the library\n"; 
 
@@ -53,27 +53,26 @@ int main()
             std::cout << "\nYou have checked in a book.\n";
             break;
         case 4:
-            /*void print_vector(std::vector<int> data)
+            void print_vector(std::vector<int> data);
             {
-                for()
+                for(int i = 0; inventory.Books.size(); i++)
                 {
-
+                    std::cout << inventory.Books[i].Id << "" << inventory.Books[i].Title << "" << inventory.Books[i].Author << std::endl;
                 }
             }
-            
-            int numBooks = totalBooks();
-            if (numBooks > 0) 
+       
+            break;
+        case 5:
+            int numBooks = inventory.Books.size();
+            if (numBooks > 0)
             {
-                //display list of books
+                std::cout << "\nThere are " << numBooks << " books in our library.\n";
             }
-            else 
+            else
             {
                 std::cout << "\nThere are no books to display.\n";
             }
-            */
-            break;
-        case 5:
-
+           
             break;
         case 6:
             std::cout << "Thank you. Goodbye.\n";
