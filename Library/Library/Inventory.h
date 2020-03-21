a6d5889bb597a4732a1588f2641a26c722d35bca
Nicole Tilbe <nicolecali2406@outlook.com>
2020-03-21 12:00:10 -0400

    cleaned up main function

--- Library/Library/Inventory.h
+++ Library/Library/Inventory.h
@@ -7,9 +7,9 @@ class Inventory
 {
 public:
 	std::vector<Book> Books;
-	void AddBook(Book book);
-	void RemoveBook(Book book);
+	void AddBook(Book book); // Add a book to the library
+	void RemoveBook(Book book); // Remove a book from the library
 	int FindBookByTitle(std::string title); //For this project we are going to assume there are no duplicate named books
-	void CheckOutBook(Book &book);
-	void CheckInBook(Book &book);
+	void CheckOutBook(Book &book); // Check out a book from the library
+	void CheckInBook(Book &book); // Check in a book to the library
 };
--- Library/Library/Library.cpp
+++ Library/Library/Library.cpp
@@ -6,6 +6,99 @@
 
 Inventory inventory; //class and object
 
+void AddBook() // Case 1
+{
+    std::cout << "Enter Book Title:\n";
+    std::string bookTitle;
+    std::getline(std::cin, bookTitle);
+
+    std::cout << "Enter Book Author:\n";
+    std::string bookAuthor;
+    std::getline(std::cin, bookAuthor);
+
+    int bookId = inventory.Books.size() + 1;
+
+    Book newBook(bookId, bookTitle, bookAuthor); //constructor
+
+    inventory.AddBook(newBook); //sends the whole book to the inventory
+
+    std::cout << "\nYou have added a new book to the library.\n";
+}
+
+
+void DisplayAllBooks() // Case 2
+{
+    std::cout << "\nID\tTitle\tAuthor" << std::endl;
+     for (int i = 0; i < inventory.Books.size(); i++) // Goes from 0 to the size of the vector (ex. if there are 10 elements in the vector, it will be 10
+     {
+             std::cout << inventory.Books[i].Id << "\t" << inventory.Books[i].Title << "\t" << inventory.Books[i].Author << std::endl; // Displays all books in inventory
+     }
+            std::cout << std::endl;
+}
+
+void CheckOutBook() // Case 3
+{
+    std::cout << "Enter a Book Title to Check Out: ";
+    std::string bookTitle;
+    std::getline(std::cin, bookTitle);
+
+    int index = (inventory.FindBookByTitle(bookTitle));
+
+    if (index >= 0)
+    {
+        if (inventory.Books[index].CheckedOut)
+        {
+            std::cout << "Book already Checked Out." << std::endl;
+            return;
+        }
+        inventory.Books[index].CheckedOut = true;
+        std::cout << "\nYou have checked out a new book.\n";
+    }
+    else
+    {
+        std::cout << "Book not Found" << std::endl;
+    }
+}
+
+void CheckInBook() //Case 4
+{
+    std::cout << "Enter a Book Title to Check In: ";
+    std::string bookTitle;
+    std::getline(std::cin, bookTitle);
+
+    int index = inventory.FindBookByTitle(bookTitle);
+
+    if (index >= 0)
+    {
+        Book foundBook = inventory.Books[index];
+
+        if (!foundBook.CheckedOut)
+        {
+            std::cout << "Book already checked in." << std::endl;
+            return;
+        }
+        inventory.Books[index].CheckedOut = false;
+        std::cout << "\nYou have checked in a book.\n";
+    }
+    else
+    {
+        std::cout << "Book not Found" << std::endl;
+    }
+}
+
+void DisplayNumBooks() // Case 5
+{
+    int numBooks = inventory.Books.size();
+    if (numBooks > 0)
+    {
+        std::cout << "\nThere are " << numBooks << " books in our library.\n";
+    }
+    else
+    {
+        std::cout << "\nThere are no books to display.\n";
+    }
+}
+
 
 int main()
 {
@@ -24,102 +117,34 @@ int main()
         std::cout << "6. Exit the library\n"; 
 
 
-        std::cin >> choice;
-        std::cin.ignore(); // This is for the getline
+        std::cin >> choice; // User enters 1-6; otherwise comes back invalid
+        std::cin.ignore(); // This is for the getline, need it to grab the entire line
 
         switch (choice)
         {
         case 1: 
         {
-            std::cout << "Enter Book Title:\n";
-            std::string bookTitle;
-            std::getline(std::cin, bookTitle);
-
-            std::cout << "Enter Book Author:\n";
-            std::string bookAuthor;
-            std::getline(std::cin, bookAuthor);
-
-            int bookId = inventory.Books.size() + 1;
-
-            Book newBook(bookId, bookTitle, bookAuthor); //constructor
-
-            inventory.AddBook(newBook); //sends the whole book to the inventory
-
-            std::cout << "\nYou have added a new book to the library.\n";
+            AddBook();
             break;
         }
         case 2:
         {
-            std::cout << "\nID\tTitle\tAuthor" << std::endl;
-            for (int i = 0; i < inventory.Books.size(); i++)
-            {
-                std::cout << inventory.Books[i].Id << "\t" << inventory.Books[i].Title << "\t" << inventory.Books[i].Author << std::endl;
-            }
-            std::cout << std::endl;
+            void DisplayAllBooks();
             break;
         }
         case 3:
         {
-            std::cout << "Enter a Book Title to Check Out: ";
-            std::string bookTitle;
-            std::getline(std::cin, bookTitle);
-           
-            int index = (inventory.FindBookByTitle(bookTitle));
-
-            if (index >= 0)
-            {
-                if (inventory.Books[index].CheckedOut)
-                {
-                    std::cout << "Book already Checked Out." << std::endl;
-                    break;
-                }
-                inventory.Books[index].CheckedOut = true;
-                std::cout << "\nYou have checked out a new book.\n";
-            }
-            else
-            {
-                std::cout << "Book not Found" << std::endl;
-            }
+            void CheckOutBook();
             break;
         }
         case 4:
         {
-            std::cout << "Enter a Book Title to Check In: ";
-            std::string bookTitle;
-            std::getline(std::cin, bookTitle);
-         
-            int index = inventory.FindBookByTitle(bookTitle);
-
-            if (index >= 0) 
-            {
-                Book foundBook = inventory.Books[index];
-
-                if (!foundBook.CheckedOut)
-                {
-                    std::cout << "Book already checked in." << std::endl;
-                    break;
-                }
-                inventory.Books[index].CheckedOut = false;
-                std::cout << "\nYou have checked in a book.\n";
-            }
-            else
-            {
-                std::cout << "Book not Found" << std::endl;
-            }
+            void CheckInBook();
             break;
         }
         case 5:
         {
-            int numBooks = inventory.Books.size();
-            if (numBooks > 0)
-            {
-                std::cout << "\nThere are " << numBooks << " books in our library.\n";
-            }
-            else
-            {
-                std::cout << "\nThere are no books to display.\n";
-            }
-
+            void DisplayNumBooks();
             break;
         }
         case 6:
@@ -131,7 +156,7 @@ int main()
         Default:
             std::cout << "Invalid Selection. Please try again." << std::endl;
         }
-    } while (choice != 6);
+    } while (choice != 6); //this will loop until you select #6 to exit the library
 
     return 0;
 }
